7ecb271bfbf7f83525cf62c2aa0876ee9dd32dd4
jimmyfr <jimmy@fresnel.cc>
2017-05-10 10:23:53 -0600

    Delete sample tasks

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -15,57 +15,7 @@ export class TaskService {
   getTasks() {
     // Simulate json response.
     const mockData = {
-      "data": [
-        {
-          "id": 0,
-          "title": "Buy groceries",
-          "description": "Get milk, eggs and pancakes!",
-          "estimate": 1.5,
-          "status": 1
-        },
-        {
-          "id": 1,
-          "title": "Finish homework",
-          "description": "Pending",
-          "estimate": 2,
-          "status": 1
-        },
-        {
-          "id": 2,
-          "title": "Add search",
-          "description": "Create a search to filter by title.",
-          "estimate": 6,
-          "status": 2
-        },
-        {
-          "id": 3,
-          "title": "Call mom",
-          "description": "Ask her about the dog.",
-          "estimate": 4.5,
-          "status": 3
-        },
-        {
-          "id": 4,
-          "title": "Run a marathon",
-          "description": "More details about this task here.",
-          "estimate": 4.5,
-          "status": 3
-        },
-        {
-          "id": 5,
-          "title": "Learn something new today",
-          "description": "Do some research on interesting things every day, and learn!",
-          "estimate": 1.25,
-          "status": 3
-        },
-        {
-          "id": 6,
-          "title": "Make tasks dragable",
-          "description": "Users should be able to drag and drop the tasks",
-          "estimate": 5.5,
-          "status": 1
-        }
-      ]
+      data: []
     };
 
     return Observable.of(mockData).map(response => response.data);
